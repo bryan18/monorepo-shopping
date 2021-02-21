0fa100419caa16af643310dd3aeab62b4d09b36c
Bryan Azofeifa Aguero <bryan18is@hotmail.com>
2021-02-21 12:24:52 -0600

    Navbar-horizontal and translate

--- libs/shared/components/src/lib/shared-components.module.ts
+++ libs/shared/components/src/lib/shared-components.module.ts
@@ -1,11 +1,50 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// ng-bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+// Components custom
+import { HeaderComponent } from './header/header.component';
+import { NavbarHorizontalComponent } from './navbar-horizontal/navbar-horizontal.component';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  console.log(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
-  imports: [CommonModule, FormsModule,
-    ReactiveFormsModule],
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule,
+    PerfectScrollbarModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  declarations: [
+    HeaderComponent,
+    NavbarHorizontalComponent
+  ],
+  exports: [
+    HeaderComponent,
+    NavbarHorizontalComponent,
+    TranslateModule
+  ]
 })
 export class SharedComponentsModule {}
